79c1f1d2ac360012c128e4171b4d49fd244a5b5a
Humpo <accollege2013@gmail.com>
2015-11-15 00:36:31 +0000

    actually made an app of stuff

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
-
+@app.route('/')
 def home():
     return render_template("./index.html")
+
+
+
 
 
 
