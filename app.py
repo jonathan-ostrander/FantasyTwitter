70b71556f12dabe905274fae8d16bf93322aa847
JonathonSchad <jschad@albany.edu>
2015-11-15 02:31:22 -0500

    db done son

--- app.py
+++ app.py
@@ -1,7 +1,50 @@
 from flask import Flask, render_template, redirect, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
+handles = db.Table('handles',
+    db.Column('team_id', db.Integer, db.ForeignKey('team.id')),
+    db.Column('handle_id', db.Integer, db.ForeignKey('handle.id'))
+)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(64), index=True, unique=True)
+    teams = db.relationship('Team', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+       
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    handles = db.relationship('Handle', secondary=handles, backref=db.backref('teams', lazy='dynamic'))
+    timestamp = db.Column(db.DateTime)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Team %r>' % (self.user)
+        
+class Handle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    cost = db.Column(db.Integer)
+    datapoints = db.relationship('HandleData', backref='handle_author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Handle %r>' % (self.name)  
+
+class HandleData(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    handle = db.Column(db.Integer, db.ForeignKey('handle.id'))
+    retweets = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<HandleData %r>' % (self.dataPt)          
 
 @app.route('/')
 def home():
