1ea3f7642b34fb3b9c82624c37df1df2a83501ab
Humpo <acomerford@albany.edu>
2015-11-15 02:33:58 -0500

    added multithreading functions to be used to database

--- app.py
+++ app.py
@@ -1,27 +1,115 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+import tweet_get
+import datetime as DT
+from time import sleep
+from multiprocessing import Pool
+import multiprocessing
+import json
 
 app = Flask(__name__)
+short_time_read = True
+_pool = None
+
+
+#update database and stuff
+def get_retweet_info(time, name, past, delta):
+	'''
+	arguments : int(seconds), String name
+	returns boolean
+
+	function will be called periodically by thread
+	and will update the database from 
+	'''
+	past_date = DT.datetime.today() - DT.timedelta(days=1)
+	return None
+
+def get_info_raw(name, past, delta):
+	'''
+	return json about the user from the name
+	'''
+
+	last_week = DT.datetime.today() - DT.timedelta(days=past['days'],
+												   hours=past['hours'],
+												   minutes=past['minutes']
+													)
+
+	return_dict = {
+		'name': name,
+		'retweets': tweet_get.getRetweets(name, last_week, delta),
+		'date_start':last_week,
+		'date_end':last_week + DT.timedelta(days=delta['days'],
+										   hours=delta['hours'],
+										   minutes=delta['minutes']
+											)
+	}
+
+	return return_dict
+
+def write_to_db(time, x):
+	past = {
+		'days':1,
+		'hours':0,
+		'minutes':0
+	}
+
+	delta = {
+		'days':0,
+		'hours':23,
+		'minutes':0
+	}
+	with open("./top100.json") as twit_users:
+		json_data = json.load(twit_users)
+		while True:
+			for user in json_data:
+				data = get_info_raw(user[1:], past, delta)
+				print data['name'], data['retweets']
+			sleep(time)
 
 
 @app.route('/')
 def home():
-    return render_template("./index.html")
+    return render_template("index.html")
 
-@app.route('/getinfo', methods=["POST"])
-def getinfo():
+@app.route('/getinfo/<name>')
+def getinfo(name):
 	'''
-	This method returns the json data from
-	a timeline from a user thingy
+	return json about the user from the name
 	'''
-	pass
+
+	last_week = DT.datetime.today() - DT.timedelta(days=1)
+	delta = 7
+
+	return_dict = {
+		'name': name,
+		'retweets': tweet_get.getRetweets(name, last_week, delta),
+		'date_start':last_week,
+		'date_end':last_week + DT.timedelta(days=delta)
+	}
+
+	return jsonify(return_dict)
+
+@app.route("/get/<int:x>")
+def do_square(x):
+
+	#f = _pool.apply_async(square,[x])
+	#r = f.get(timeout=2)
+	#return 'Result is %d'%r
+	return jsonify({'yay':69})
 
 @app.route('/buytweeter',methods=["POST"])
 def buytweeter():
 	'''
 	this method buys a tweeter 
 	'''
-
+	pass
 
 
 if __name__ == "__main__":
-    app.run()
+	_pool = Pool(processes=1)
+	try:
+		p = multiprocessing.Process(target=write_to_db, args=(1,10))
+		p.start()
+		app.run(debug=True)
+	except KeyboardInterrupt:
+		_pool.close()
+		_pool.join()
--- tweet_get.py
+++ tweet_get.py
@@ -1,21 +1,22 @@
 import tweepy
 import datetime as DT
 
-consumer_key = 'asdaasd'
-consumer_secret = 'asd'
-access_token = '700001043-asd'
-access_token_secret = 'asf'
+consumer_key = 'asdf'
+consumer_secret = 'sdf'
+access_token = 'asdf'
+access_token_secret = 'asdf'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 def getRetweets(name, begin, delta):
-    after = begin + DT.timedelta(days=delta)
+    after = begin + DT.timedelta(days=delta['days'],
+                               hours=delta['hours'],
+                               minutes=delta['minutes'])
     user = api.get_user(name)
-    tweets = api.user_timeline(user.id, count=200, include_rts=False)
-   
-    print "date", tweets[-1].created_at
+    #tweets = api.user_timeline(user.id, count=200, include_rts=False)
+    tweets = tweepy.Cursor(api.user_timeline, count=200).pages(16)
 
     retweet_sum = 0
     '''
@@ -32,7 +33,8 @@ def getRetweets(name, begin, delta):
    	#loop through each tweet and add to sum
     
     while tweets[-1].created_at > begin:
-        tweets = api.user_timeline(user.id, max_id=tweets[-1].id, count=200, include_rts=False)
+        #tweets = api.user_timeline(user.id, max_id=tweets[-1].id, count=200, include_rts=False)
+        tweets = tweepy.Cursor(api.user_timeline, count=200).pages(16)
         for tweet in tweets:
 	    	if after > tweet.created_at > begin:
 	    		retweet_sum += tweet.retweet_count
@@ -40,5 +42,6 @@ def getRetweets(name, begin, delta):
     return retweet_sum
     
 if __name__=="__main__":
-    last_week = DT.datetime.today() - DT.timedelta(days=1)
-    print getRetweets("realDonaldTrump", last_week, 7)
+    pass
+    #last_week = DT.datetime.today() - DT.timedelta(days=1)
+    #print getRetweets("realDonaldTrump", last_week, 7)
